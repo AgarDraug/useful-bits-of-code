05dfd55c15cc726bf6f3f78f3a30149ee8449e44
Andre Silva <andre.2107.silva@gmail.com>
2017-07-10 22:44:54 +0100

    Added let and const

---
+++ ES6/Let and Const/main.js
@@ -0,0 +1,40 @@
+/*function testVar(){
+    var a = 30;
+    if(true){
+        var a = 50;
+        console.log("Testing with var: inside if - " + a);
+    }
+    console.log("after if - " + a);
+}
+
+//testVar();
+
+function testLet(){
+    let b = 30;
+    if(true){
+        let b = 50;
+        console.log("Testing with let: inside if - " + b);
+    }
+    console.log("after if - " + b);
+}
+
+//testLet();
+
+for(var i = 0; i< 10; i++){
+    console.log(i);
+}
+console.log(i);
+console.log("using let...");
+for(let e = 0; e< 10; e++){
+    console.log(e);
+}
+console.log(e);*/
+
+const colors = [];
+
+colors.push('red');
+colors.push('blue');
+
+colors = 'green';
+
+console.log(colors);
