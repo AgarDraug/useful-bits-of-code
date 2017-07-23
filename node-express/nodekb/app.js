681eba3cb4fa115d3a07c60b6e98e2e588a2792c
André Silva <andre.2107.silva@gmail.com>
2017-07-23 16:00:53 +0000

    Added Node Express Project

---
+++ node-express/nodekb/app.js
@@ -0,0 +1,135 @@
+const express = require('express');
+const path = require('path');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+// Coonect to DB
+mongoose.connect('mongodb://localhost/nodekb', {
+    useMongoClient:true
+});
+let db = mongoose.connection;
+
+// Check Connection
+
+db.once('open', () => {
+    console.log('connected to MongoDB');
+});
+
+// Check for db errors
+db.on('error', (err) => {
+    console.log(err);
+});
+
+
+// Init App
+const app = express();
+
+// Bring in Models
+let Article = require('./models/article');
+
+// Load View Engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+//  Body Parser Middleware 
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json());
+
+// Set Public Folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Home Route
+app.get('/', (req, res) => {
+    Article.find({}, (err, articles) => {
+        if(err){
+            console.log(err);
+        }else{
+      res.render('index', {
+        title:'Articles',
+        articles: articles
+            });
+        }  
+    });  
+});
+
+// Get Single Article
+app.get('/articles/:id', (req, res) => {
+    Article.findById(req.params.id, (err, article) => {
+        res.render('article', {
+            article:article
+        });
+    });
+});
+
+// Load Edit Form
+app.get('/articles/edit/:id', (req, res) => {
+    Article.findById(req.params.id, (err, article) => {
+        res.render('edit_article', {
+            title:'Edit Article',
+            article:article
+        });
+    });
+});
+
+// Catch Edit Form
+app.post('/articles/edit/:id', (req, res) => {
+    let article = {};
+    article.title = req.body.title;
+    article.author = req.body.author;
+    article.body = req.body.body;
+
+    let query = {_id:req.params.id}
+
+    Article.update(query, article, (err) => {
+        if(err){
+            console.log(err);
+        }else{
+            res.redirect('/');
+        }
+    });
+});
+
+// Delete Request
+app.delete('/articles/:id', (req, res) => {
+    let query = {_id:req.params.id}
+
+    Article.remove(query, (err) => {
+        if(err){
+            console.log(err);
+        }else{
+            res.send('Success');
+        }
+    });
+});
+
+// Add Route
+app.get('/articles/add', (req, res) => {
+    res.render('add_article', {
+        title:'Add Article'
+    });
+});
+
+// Add Submit POST Route
+app.post('/articles/add', (req, res) =>{
+    let article = new Article();
+    article.title = req.body.title;
+    article.author = req.body.author;
+    article.body = req.body.body;
+
+    article.save((err) => {
+        if(err){
+            console.log(err);
+            return;
+        }else{
+            res.redirect('/');
+        }
+    });
+});
+
+// Start Server
+app.listen(3000, () =>{
+    console.log('Server listening on port 3k...');
+});
